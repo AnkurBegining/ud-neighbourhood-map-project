602c4cf66647692d56a6df5014e310cc97c67ff0
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2018-01-22 00:39:24 +0530

    feat: temple location i.e latitude and longitude added inarray object

--- js/base.js
+++ js/base.js
@@ -6,3 +6,24 @@ function initMap() {
     zoom: 13
   });
 }
+
+var markerList = [
+	{
+		title: 'Swaminarayan Akshardham', 
+		location: {lat: 23.2293087, lng: 72.6741314}
+	},
+ 	
+ 	{
+ 		title: 'Ambaji temple', 
+ 		location: {lat: 23.2041136, lng: 72.6308771}
+ 	},
+ 	
+ 	{
+ 		title: 'Dholeshwer Mahadev Temple', 
+ 		location: {lat: 23.181022, lng: 72.645094}
+ 	},
+ 
+ 	{   title: 'BAPS Swaminarayan Hari Mandir', 
+ 		location: {lat: 23.2286087, lng: 72.6211846}
+ 	}
+];
