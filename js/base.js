250ad695c25e8a2c4efe1de3645b04a2b9f787cd
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2018-01-22 11:59:06 +0530

    fix: initial mobile responsive

--- js/base.js
+++ js/base.js
@@ -3,7 +3,7 @@ function initMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 23.2293105, lng: 72.6653764},
-    zoom: 13
+    zoom: 16
   });
 
 
